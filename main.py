7e3a6e9c3699fe1795768a8a5f809e98865b40d0
María Álvarez Jiménez <maj20091999@gmail.com>
2023-11-15 11:22:27 +0100

    ex 3

--- main.py
+++ main.py
@@ -25,20 +25,41 @@ def multiplication_table():
 
 
 def basic_calculator():
-  """
-    Using a while/for loops, implement a basic calculator.
-    1. Enter the first number: 10
-    2. Enter an operator (+, -, *, /): +
-    3. Enter the second number: 20
-    4. print 10 + 20 => Result: 30
-  """
-  num1 = input("Enter the first number: ")
-  operator = input("Enter an operator (+, -, *, /): ")
-  num2 = input("Enter the second number: ")
+    def suma(num1, num2):
+        return num1 + num2
 
-  result = None # fix code
+    def resta(num1, num2):
+        return num1 - num2
 
-  print("{num1} {operator} {num2} => Result:", result)
+    def multiplicacion(num1, num2):
+        return num1 * num2
+
+    def division(num1, num2):
+        return num1 / num2
+
+    print("Por favor, elige una operación:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+
+    opcion = input("Ingresa una opción (1/2/3/4): ")
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    if opcion == '1':
+        print(num1, "+", num2, "=", suma(num1, num2))
+    elif opcion == '2':
+        print(num1, "-", num2, "=", resta(num1, num2))
+    elif opcion == '3':
+        print(num1, "*", num2, "=", multiplicacion(num1, num2))
+    elif opcion == '4':
+        if num2 == 0:
+            print("No se puede dividir por cero.")
+        else:
+            print(num1, "/", num2, "=", division(num1, num2))
+    else:
+        print("Opción inválida")
 
 
 def main():
